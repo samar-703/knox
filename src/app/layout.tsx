3e45a75e81057403fe6cf4102c464e47859bb82d
samaR <hello.samar7@gmail.com>
2026-01-21 18:19:43 +0000

    added clerk package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import "./globals.css";
+import { Sign } from "crypto";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,19 +37,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${plexMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.variable} ${plexMono.variable} antialiased`}
         >
-          {children}
-          </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-rose-500 text-white p-2 rounded">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn> 
+              <UserButton />
+            </SignedIn>
+            </header>
+            {children}
+            </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
