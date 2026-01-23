1b9f08ef5c774d9c83da7626145106741636b545
samaR <hello.samar7@gmail.com>
2026-01-23 15:46:07 +0000

    refactor page component to fetch and display tasks from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import { Button } from "@/components/ui/button";
+"use client"; 
 
-const something = () =>{
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+const X = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
-    <div>
-      <Button>
-        Click me daddy
-        </Button>
+    <div className="flex flex-col gap-2" >
+      {tasks?.map((task) => (
+        <div className="border rounded p-2 flex flex-col" key={task._id}>
+          <p>{task.text}</p>
+          <p>Is completed: {`${task.isCompleted}`}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default something;
+export default X;
