30dac97424d2a5f78f28a6362c03049a3ee97edc
samaR <hello.samar7@gmail.com>
2026-02-06 18:30:45 +0000

    feat: add Layout component for project ID handling in project pages

---
+++ src/app/projects/[projectId]/layout.tsx
@@ -0,0 +1,20 @@
+import { ProjectIdLayout } from "@/features/projects/components/project-id-layout";
+
+const Layout =  async({
+  children,
+  params,
+} : {
+  children: React.ReactNode;
+  params: Promise<{ projectId: string }>;
+}) => {
+  const { projectId } = await params; 
+  return (
+    <ProjectIdLayout
+      projectId={projectId}
+    >
+      {children}
+    </ProjectIdLayout>
+  );
+}
+
+export default Layout;
