06e6c72c8e1f946c6482b266926991cf86e00ea9
samaR <hello.samar7@gmail.com>
2026-02-06 21:15:31 +0000

    feat: add ProjectIdView component for project-specific view management

--- src/features/projects/hooks/use-projects.ts
+++ src/features/projects/hooks/use-projects.ts
@@ -45,3 +45,37 @@ export const useCreateProject = () => {
     }
   )
 };
+
+export const useRenameProject = (projectId: Id<"projects">) => {
+  return useMutation(api.projects.rename).withOptimisticUpdate(
+    (localStore, args) => {
+      const existingProject = localStore.getQuery(api.projects.getById, { id: projectId} );
+
+      if (existingProject !== undefined && existingProject !== null) {
+        localStore.setQuery(
+          api.projects.getById,
+          { id: projectId },
+          {
+            ...existingProject,
+            name: args.name,
+            updatedAt: Date.now()
+          }
+        );
+      }
+
+      const existingProjects = localStore.getQuery(api.projects.get);
+
+      if (existingProjects !== undefined){
+        localStore.setQuery(
+          api.projects.get,
+          {},
+          existingProjects.map((project) => {
+            return project._id === args.id
+              ? { ...project, name: args.name, updatedAt:Date.now() }
+              : project;
+          })
+        );
+      }
+    }
+  )
+};
