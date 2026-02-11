33f699a1aa1e13b52103f613734b8834f9b631ee
samaR <hello.samar7@gmail.com>
2026-02-11 18:11:29 +0000

    feat: editor option added

--- src/features/projects/components/project-id-view.tsx
+++ src/features/projects/components/project-id-view.tsx
@@ -1,10 +1,16 @@
 "use client";
 
 import { useState } from "react";
+import { Allotment } from "allotment";
 import { FaGithub } from "react-icons/fa";
 import { cn } from "@/lib/utils";
 import { Id } from "../../../../convex/_generated/dataModel";
 
+
+const MIN_SIDEBAR_WIDTH = 200;
+const MAX_SIDEBAR_WIDTH = 800;
+const DEFAULT_SIDEBAR_WIDTH = 350;
+const DEFAULT_MAIN_SIZE = 1000;
 
 
 const Tab = ({
@@ -65,7 +71,20 @@ export const ProjectIdView = ({
           "absolute inset-0",
           activeView === "editor" ? "visible": "invisible"
         )}>
-          <div>Editor</div>
+          <Allotment defaultSizes={[DEFAULT_SIDEBAR_WIDTH, DEFAULT_MAIN_SIZE]}>
+            <Allotment.Pane
+              snap
+              minSize={MIN_SIDEBAR_WIDTH}
+              maxSize={MAX_SIDEBAR_WIDTH}
+              preferredSize={DEFAULT_SIDEBAR_WIDTH}
+            >
+              
+              <p>File explorer</p>
+            </Allotment.Pane>
+            <Allotment.Pane>
+              <p>Editor view</p>
+            </Allotment.Pane>
+          </Allotment>
         </div>
         <div className= {cn(
           "absolute inset-0",
